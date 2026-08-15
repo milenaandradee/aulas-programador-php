2bf7743ecb877fa9cf01c4cce2d554d6167ad2e5
Milena Andrade Abreu <abreumilena145@gmail.com>
2026-08-14 21:24:46 -0300

    adicionar exercicios da aula sobre operadores aritmeticos

--- 01-introducao-ao-php/index.php
+++ 01-introducao-ao-php/index.php
@@ -5,4 +5,23 @@ $quantidadeDeHorasTrabalhadas = 144;
 
 $salario = $valorDaHora * $quantidadeDeHorasTrabalhadas;
 
-echo "O salário total é de R$ {$salario}";
+echo "<p>O salário total é de R$ {$salario}</p>";
+
+
+$quantidadeDeAmigos = 4;
+$valorDaComanda = 177.54;
+
+$quantidadeDePessoas = $quantidadeDeAmigos++;
+
+$valorPorPessoa = $valorDaComanda / $quantidadeDePessoas;
+
+echo "<p>O valor que cada pessoa irá pagar é de R$ {$valorPorPessoa}</p>";
+
+
+
+$quantidadeDedias = 20;
+$quantidadeDeConsumoDiario = 1;
+
+$dias = $quantidadeDedias / $quantidadeDeConsumoDiario;
+
+echo "A quantidade de dias que o Bob dura comendo um saco de ração de 20 quilos é {$dias}";
