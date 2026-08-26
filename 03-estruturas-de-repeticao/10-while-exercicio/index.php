c85925ece0bdd0ed8bbad6e41fc728aa5a026ae5
Milena Andrade Abreu <abreumilena145@gmail.com>
2026-08-26 20:35:20 -0300

    adicionar alteração de resolução de exercício na pasta 10-while-exercicio

--- 03-estruturas-de-repeticao/10-while-exercicio/index.php
+++ 03-estruturas-de-repeticao/10-while-exercicio/index.php
@@ -19,8 +19,24 @@ echo "<p>Já é a sua vez!</p>";
 
 echo "<h1>Exercício - 02</h1>";
 
-$capacidadeDeAlunos = 20;
-$entradaDeAlunos = 3;
+const CAPACIDADE_MAXIMA = 20;
+$alunosNaSala = 1;
+$alunosEmAtraso = 0;
 
-while ($entradaDeAlunos >= $capacidadeDeAlunos);
+while ($alunosNaSala <= CAPACIDADE_MAXIMA){
+    
+if ($alunosNaSala % 3 === 0){
+        echo "<p>Bloqueado: Aluno {$alunosNaSala} está com a mensalidade em atraso.</p>";
+        $alunosEmAtraso++; // quantidade de alunos em atraso 
+        $alunosNaSala++;
+        continue;
+    }
+    echo "<p>Aluno liberado. Entrada {$alunosNaSala} de " . CAPACIDADE_MAXIMA . "</p>";
+    $alunosNaSala++;
+}
+    
+
+echo "<p><strong> Capacidade Maxima atingida </strong></p>";
+echo "<p>Total de bloqueados realizadas: {$alunosEmAtraso}</p>";
+
 
