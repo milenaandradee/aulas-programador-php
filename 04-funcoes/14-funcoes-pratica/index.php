b14959715297271cd457b85c5c87632df4d22442
Milena Andrade Abreu <abreumilena145@gmail.com>
2026-09-01 20:50:29 -0300

    feat: adicionar exercícios na pasta 14-funcoes-pratica

--- 04-funcoes/14-funcoes-pratica/index.php
+++ 04-funcoes/14-funcoes-pratica/index.php
@@ -35,9 +35,55 @@ calcularIdade(1995);
 
 // com parâmetros obrigatórios e opcionais
 
-function saudacaoPersonalizada(string $nome, string $saudacao = "Seja muitobem vindo(a)!"){
+function saudacaoPersonalizada(string $nome, string $saudacao = "Seja muito bem vindo(a)!"){
     echo "<p>Olá, {$nome}. {$saudacao}</p>";
 }
 
 saudacaoPersonalizada ("Elisabeth");
-saudacaoPersonalizada ("Milena");
+saudacaoPersonalizada ("Milena", "Chegou o seu pedido");
+
+//Aprendendo função 
+
+function verificarMaioridade(int $anoNascimento){
+    $idade = 2026 - $anoNascimento;
+
+    if($idade < 18){
+        return false;
+    }else if($idade >= 18){
+        return true;
+}}
+
+if(verificarMaioridade(2004) === true){
+     echo "<p>Você é MAIOR de idade!</p>";
+}else{
+    echo "<p>Você é MENOR de idade!</p>";}
+
+function calculaIdade(int $anoNascimento, string $nome = ""){
+    $idade = 2026 - $anoNascimento;
+    return $idade;
+}
+
+$idadeDaFabricia = calculaIdade(1998, "Fabricia");
+echo "A Fabrícia tem {$idadeDaFabricia} anos de idade";
+echo calculaIdade(2011) . " anos de idade";
+
+// novidade
+
+echo "Olá, mundo!";
+
+function echo_p(string $string){
+    echo "<p>{$string}</p>";
+}
+
+echo_p("Olá, mundo!");
+echo_p("A Fabrícia tem {$idadeDaFabricia} anos de idade");
+echo_P(calculaIdade(2011) . " anos de idade");
+
+//
+
+$comissao = 20000 * 0.1;
+
+echo "R$ " . number_format($comissao, 2, ",", ".");
+
+// R$ 152,00
+// R$ 1.152,00
