07012c32f7b58dd29aa178f619c22c7042d5c660
Milena Andrade Abreu <abreumilena145@gmail.com>
2026-09-09 21:45:13 -0300

    feat: adicionar pasta 18-fornularios-get-praticar

---
+++ 06-formularios-parte1/18-fornularios-get-praticar/busca.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busca de livro</title>
+</head>
+<body>
+    
+    <form action="index.php" method="GET">
+        <label for="livro">Procure pelo titulo do livro</label>
+        <input type="text" name="livro" id="livro">
+        <br>
+        <button type="submit">Buscar</button>
+</form>
+</body>
+</html>
---
+++ 06-formularios-parte1/18-fornularios-get-praticar/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . "/../../senac/senac.php";
+
+var_dump(
+    $_GET
+);
